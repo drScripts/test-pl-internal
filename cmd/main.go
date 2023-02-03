784d3755d3b627eb523a3ff7bc6b471def01e4e7
drScripts <nathanael.vd@gmail.com>
2023-02-03 18:44:53 +0700

    test2

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,7 @@ import (
 func apiMiddleware(fc echo.HandlerFunc) echo.HandlerFunc {
 	return func(context echo.Context) error {
 		context.Request().Header.Set("gateway", "true")
-		fmt.Println("HAIII TEST")
+		fmt.Println("HAIII TESTs")
 		return fc(context)
 
 	}
